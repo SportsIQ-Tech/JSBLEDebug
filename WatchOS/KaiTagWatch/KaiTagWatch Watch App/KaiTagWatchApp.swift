b6ce821ee9eed02736013ad134925bf1517f30a9
Paulius Velesko <pvelesko@gmail.com>
2025-04-22 14:45:16 +0300

    initial watchos

---
+++ WatchOS/KaiTagWatch/KaiTagWatch Watch App/KaiTagWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaiTagWatchApp.swift
+//  KaiTagWatch Watch App
+//
+//  Created by Paulius Velesko on 22/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct KaiTagWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchOS/KaiTagWatch/KaiTagWatch/KaiTagWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaiTagWatchApp.swift
+//  KaiTagWatch
+//
+//  Created by Paulius Velesko on 22/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct KaiTagWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
