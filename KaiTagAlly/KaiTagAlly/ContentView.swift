5ce2a7425c78b2c1145b188ac24d84cec0b698b4
Paulius Velesko <pvelesko@gmail.com>
2025-05-02 16:34:42 +0300

    kaitag ally

---
+++ KaiTagAlly/KaiTagAlly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KaiTagAlly
+//
+//  Created by Paulius Velesko on 02/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KaiTagAlly/KaiTagAlly/KaiTagAllyApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaiTagAllyApp.swift
+//  KaiTagAlly
+//
+//  Created by Paulius Velesko on 02/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct KaiTagAllyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
